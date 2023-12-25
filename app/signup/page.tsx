23156de398409cbade280e11377ab817c6669152
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-12-25 23:06:02 +0530

    feature : signup page added

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,13 +2,15 @@
 import Image from "next/image"
 import { spaceGrotesk } from "../fonts"
 import { MdArrowOutward } from "react-icons/md";
-import { FaEye } from "react-icons/fa";
+import { FaEye,FaEyeSlash } from "react-icons/fa";
+
 import Link from "next/link";
 import { useState } from "react";
 
 const page = () => {
   const [email,setEmail] = useState('');
   const [password,setPassword] =useState('');
+  const [eyeClick,setEyeClick] = useState(true);
 
   const handleClick = ()=>{
     console.log("Email is "+ email);
@@ -19,7 +21,7 @@ const page = () => {
       <div className="w-1/2 h-screen flex items-center justify-center transition-all ease-out duration-500 ">
         <div  className=" group  flex justify-center items-center">
         {/* <div className="text-4xl  text-black group-hover:text-5xl transition-all ease-in-out duration-500 font-bold">NAMMADA LOGO</div> */}
-        <Image src={'/../images/login.png'} alt="login" width={1800} height={1800} className="scale-110">
+        <Image src={'/../images/signup.png'} alt="signup" width={1800} height={1800} className="scale-110">
 
         </Image>
         </div>
@@ -31,7 +33,7 @@ const page = () => {
         </div>
         <div className="text-gray-950 text-2xl font-medium flex-col mb-3 text-left w-1/2 justify-start flex ">
   
-          Nice to see you again.
+          Let's go...
   
 
         </div>
@@ -39,15 +41,20 @@ const page = () => {
           <label htmlFor="email" className="text-xl my-3 font-normal ml-2">Email</label>
           <input type="text" id="email" placeholder="Email" value={email} onChange={(e)=>{setEmail(e.target.value)}}  className="inputField"/>
           <label htmlFor="password" className="text-xl  mb-2 mt-3 font-normal ml-2">Password</label>
-          <input type="password" id="password" placeholder="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="inputField"></input>
+        <div className="w-full relative flex">
+        <input type={eyeClick? 'password':'text'} id="password" placeholder="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="inputField"></input>
+        {
+          eyeClick?<FaEye onClick={()=>{setEyeClick(!eyeClick)}} className="absolute right-0 mr-4 mt-3 text-xl text-gray-950 opacity-50 hover:opacity-100 cursor-pointer"/> :<FaEyeSlash onClick={()=>{setEyeClick(!eyeClick)}} className="absolute right-0 mr-4 mt-3 text-xl text-gray-950 opacity-50 hover:opacity-100 cursor-pointer"/>
+        }
+        </div>
         </div>
         <div className="flex flex-col mt-4 gap-4 w-[50%]">
-          <a href="" className="text-right text-blue-700 hover:text-blue-500 mb-3">Forgot Password?</a>
-        <button className="bg-[#f4fd6b] hover:bg-[#faffaf] transition-all duration-100 ease text-xxl py-4 px-6  rounded-lg" onClick={handleClick}>Log In</button>
+          {/* <a href="" className="text-right text-blue-700 hover:text-blue-500 mb-3">Forgot Password?</a> */}
+        <button className="bg-[#f4fd6b] hover:bg-[#faffaf] transition-all duration-100 ease text-xxl py-4 px-6  rounded-lg" onClick={handleClick}>Sign Up</button>
         </div>
         <div className="flex justify-between items-center w-[50%] my-5 ">
           <div className="bg-gray-950 w-[80px] h-[1px] opacity-30"></div>
-          Or login with
+          Or signup using
           <div className="bg-gray-950 w-[80px] h-[1px] opacity-30"></div>
         </div>
         <div className=" flex justify-between w-[50%] ">
@@ -55,9 +62,9 @@ const page = () => {
           <button className="bg-blue-300 w-[48%]  py-3 hover:bg-blue-200 rounded-lg">GitHub</button>
         </div>
         <div className="  w-1/2 mt-10 flex  justify-center items-center">
-          <p className="mr-2">Newbie??</p>
+          <p className="mr-2">Already an user?</p>
           <button  className=" mr-1 flex rounded-full px-3 py-[2px] text-sm justify-center items-center bg-transparent border border-gray-950">
-            <Link href={'/signup'} >Sign Up</Link>
+            <Link href={'/login'} >Log In</Link>
           </button>
           <div className="bg-[#f4fd6b] w-7 h-7 flex justify-center items-center rounded-full text-black"><MdArrowOutward /></div>
         </div>
