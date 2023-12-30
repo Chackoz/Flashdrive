2b375b48f360fd3b82991cb41b61cb4d1fc33bd1
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-12-30 17:56:26 +0530

    update : signup made responsive

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -47,10 +47,10 @@ export default function Page() {
 
   return (
     <div
-      className={`  relative flex justify-center items-center min-w-full bg-gray-50`}
+      className={`h-full md:min-h-screen relative overflow-hidden flex justify-center items-center min-w-full bg-gray-50`}
     >
-      <div className="md:flex hidden w-1/2 h-screen  items-center justify-center transition-all ease-out duration-500 ">
-        <div className=" group  flex justify-center items-center">
+         <div className="hidden md:flex md:w-1/2 h-screen items-center justify-center transition-all ease-out duration-500 ">
+        <div className=" group   justify-center items-center">
           {/* <div className="text-4xl  text-black group-hover:text-5xl transition-all ease-in-out duration-500 font-bold">NAMMADA LOGO</div> */}
           <Image
             src={signupPic}
@@ -62,13 +62,13 @@ export default function Page() {
         </div>
       </div>
       {/* <div className="w-[1px] absolute bg-gray-950 opacity-30 h-[90%]"></div> */}
-      <div className="md:w-1/2 h-screen flex flex-col items-center justify-center">
-        <div className="flex md:w-[50%] font-poppins text-4xl justify-start items-start">
-          <img src="/logo.png" className="w-[80px] -mt-4 -ml-6" />
-          <div className="">Flash Drive</div>
+      <div className="md:w-1/2 w-full flex flex-col items-center justify-center">
+        <div className="flex md:w-[50%] font-poppins text-4xl justify-items-start -ml-20 md:-ml-12 items-center  mb-5 ">
+          <img src="/logo.png" className="w-[80px] scale-150 " />
+          <div className="font-bold -ml-3">Flash Drive</div>
         </div>
-        <div className="text-gray-950 text-2xl font-medium flex-col mb-3 text-left w-1/2 justify-start flex ">
-          Let &apos;s go...
+        <div className="text-gray-950 md:text-2xl text-xl opacity-80 font-medium flex-col mb-3 text-left md:w-1/2 w-full md:px-0 px-8 justify-start flex">
+       Let's go...
         </div>
         <div className="flex-col flex md:w-1/2   justify-items-start items-start ">
           {/* <label htmlFor="name" className="text-xl my-3 font-normal ml-2">
@@ -144,21 +144,21 @@ export default function Page() {
             )}
           </div>
         </div>
-        <div className="flex flex-col mt-4 gap-4 w-[50%]">
+        <div className="flex flex-col mt-4 gap-4 md:w-[50%] w-full">
           {/* <a href="" className="text-right text-blue-700 hover:text-blue-500 mb-3">Forgot Password?</a> */}
           <button
-            className="bg-[#f4fd6b] hover:bg-[#faffaf] transition-all duration-100 ease text-xxl py-4 px-6  rounded-lg"
+            className="bg-[#f4fd6b] hover:bg-[#faffaf] transition-all duration-100 ease text-xxl py-4 md:px-6 mx-6 md:mx-0  rounded-lg"
             onClick={handleSignUp}
           >
             Sign Up
           </button>
         </div>
-        <div className="flex justify-between items-center w-[50%] my-5 ">
+        <div className="flex justify-between  items-center w-full md:px-0 px-6 md:w-[50%] my-5 ">
           <div className="bg-gray-950 w-[80px] h-[1px] opacity-30"></div>
           Or signup using
           <div className="bg-gray-950 w-[80px] h-[1px] opacity-30"></div>
         </div>
-        <div className=" flex justify-between w-[50%] ">
+        <div className=" flex justify-between  w-full md:px-0 px-6 md:w-[50%] ">
           <button className="bg-blue-300 w-[48%] py-3 hover:bg-blue-200 rounded-lg">
             Google
           </button>
